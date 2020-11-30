e6325b60ed8f89ea573cade0ffe7495c904c3922
Thomas Damiani <46034253+tomyyyD@users.noreply.github.com>
2020-11-30 11:30:10 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,64 @@
+const cursor = document.querySelector('.cursor');
+const titleContainer = document.querySelector('.titleContainer')
+const running = document.querySelector('.running')
+const runningContainer = document.querySelector('.runningContainer')
+const coding = document.querySelector('.coding')
+const codingContainer = document.querySelector('.codingContainer')
+const titleImage = document.querySelector('#titleImage')
+
+// document.addEventListener('mousemove', e => {
+//     cursor.setAttribute('style', `top: ${e.pageY - (cursor.scrollHeight/2)}px; left: ${e.pageX - (cursor.scrollWidth/2)}px`);
+// })
+
+$(window).on('load', function() {
+    $(window).on('mousemove', function(e){
+        //console.log(e.pageX)
+        cursor.setAttribute('style', `top: ${e.pageY - (cursor.scrollHeight/2)}px; left: ${e.pageX - (cursor.scrollWidth/2)}px`)
+    })
+    $(window).on('scroll', function(e){
+
+        var bottom = $(this).scrollTop() + $(this).innerHeight();
+        //console.log(bottom)
+        $('#titleImage').each(function(i, obj){
+            if (window.scrollY < titleContainer.scrollHeight){
+                //console.log("I going")
+                obj.setAttribute('style', `transform: rotate(${window.scrollY/2}deg)`)
+            }
+        })
+        $('.fade').each(function(){
+            //location of element
+            //console.log($(this))
+            var objectBottom = $(this).offset().top + $(this).outerHeight();
+
+            if (objectBottom < bottom){
+                if ($(this).css('opacity')==0) {
+                    $(this).fadeTo(500,1);
+                }
+            }else{
+                if ($(this).css('opacity')==1){
+                    $(this).fadeTo(500, 0)
+                }
+            }
+        })
+        $('.subtextContainer').each(function(i, obj){
+            var objectTop = $(this).offset().top
+            //console.log(objectTop)
+            //console.log(origin)
+            if (objectTop < bottom){
+                //console.log(obj);
+                obj.setAttribute("style", 'transform: translateX(0%)');
+            }
+            else{
+                obj.setAttribute('style', 'transform: null')
+            }
+        })
+    }).scroll();
+})
+
+function scrollFunction(pos){
+    window.scrollTo({
+        top: pos,
+        left: 0,
+        behavior: 'smooth'
+    })
+}
